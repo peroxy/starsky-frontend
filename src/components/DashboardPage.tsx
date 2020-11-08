56cc225c025831a6a5b58406d6f399e9718ad72d
peroxy <peroxy@users.noreply.github.com>
2020-11-08 21:12:48 +0100

    Helmet title

--- src/components/DashboardPage.tsx
+++ src/components/DashboardPage.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import {Helmet} from "react-helmet";
 
 export default class DashboardPage extends React.Component<any, any> {
     render() {
         return (
           <div>
+              <Helmet title={"Starsky | Dashboard"}/>
               <h1>Dashboard</h1>
               <h2>Welcome to your dashboard!</h2>
           </div>
