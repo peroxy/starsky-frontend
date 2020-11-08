34c1eefbf427dfcaadb98c721c8affd77d8cd162
peroxy <peroxy@users.noreply.github.com>
2020-11-08 21:10:32 +0100

    Added register route

--- src/routing/routeConstants.ts
+++ src/routing/routeConstants.ts
@@ -1,5 +1,6 @@
 export const HOME_ROUTE : string = "/";
 export const LOGIN_ROUTE : string = "/login";
+export const REGISTER_ROUTE : string = "/register";
 
 export const APP_ROUTE : string = "/app"
 export const DASHBOARD_ROUTE : string = `${APP_ROUTE}/dashboard`;
