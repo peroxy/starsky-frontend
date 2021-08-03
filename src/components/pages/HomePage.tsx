451a9c6401a57fb395ef76d389790c001a51d7cf
peroxy <peroxy@users.noreply.github.com>
2021-08-03 19:37:50 +0200

    Replaced stock home page text

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -25,10 +25,6 @@ interface IProps {
     authenticatedUser: UserResponse | null;
 }
 
-/* Heads up!
- * HomepageHeading uses inline styling, however it's not the best practice. Use CSS or styled
- * components for such things.
- */
 function HomepageHeading(props: IProps) {
     return (
         <Container text>
@@ -172,10 +168,6 @@ const MobileContainer: React.FC<IUserProps> = ({ children, authenticatedUser }) 
 };
 
 const ResponsiveContainer: React.FC<IUserProps> = ({ children, authenticatedUser }) => (
-    /* Heads up!
-     * For large applications it may not be best option to put all page into these containers at
-     * they will be rendered twice for SSR.
-     */
     <MediaContextProvider>
         <DesktopContainer authenticatedUser={authenticatedUser}>{children}</DesktopContainer>
         <MobileContainer authenticatedUser={authenticatedUser}>{children}</MobileContainer>
@@ -223,26 +215,21 @@ const HomePage: React.FC = () => {
                             <Grid.Row>
                                 <Grid.Column width={8}>
                                     <Header as="h3" style={{ fontSize: '2em' }}>
-                                        We Help Companies and Companions
+                                        We Help Companies and Employees
                                     </Header>
                                     <p style={{ fontSize: '1.33em' }}>
-                                        We can give your company superpowers to do things that they never thought possible. Let us delight your customers and
-                                        empower your needs... through pure data analytics.
+                                        We can give your company superpowers to do things that they never thought possible. Let us delight your employees and
+                                        empower your needs... through automatic algorithmic scheduling.
                                     </p>
                                     <Header as="h3" style={{ fontSize: '2em' }}>
-                                        We Make Bananas That Can Dance
+                                        We Make Schedules That Are Perfect
                                     </Header>
                                     <p style={{ fontSize: '1.33em' }}>
-                                        Yes thats right, you thought it was the stuff of dreams, but even bananas can be bioengineered.
+                                        Yes thats right, you thought it was the stuff of dreams, but even schedules can be engineered to perfection.
                                     </p>
                                 </Grid.Column>
                                 <Grid.Column floated="right" width={6}>
                                     <Image rounded size="large" src={logo} />
-                                </Grid.Column>
-                            </Grid.Row>
-                            <Grid.Row>
-                                <Grid.Column textAlign="center">
-                                    <Button size="huge">Check Them Out</Button>
                                 </Grid.Column>
                             </Grid.Row>
                         </Grid>
@@ -255,15 +242,18 @@ const HomePage: React.FC = () => {
                                     <Header as="h3" style={{ fontSize: '2em' }}>
                                         What a Company
                                     </Header>
-                                    <p style={{ fontSize: '1.33em' }}>That is what they all say about us</p>
+                                    <p style={{ fontSize: '1.33em' }}>
+                                        <Icon name={'user circle outline'} />
+                                        <b>Harold C. Dobey</b> - Police Captain
+                                    </p>
                                 </Grid.Column>
                                 <Grid.Column style={{ paddingBottom: '5em', paddingTop: '5em' }}>
                                     <Header as="h3" style={{ fontSize: '2em' }}>
-                                        I shouldnt have gone with their competitor.
+                                        I should not have gone with their competitor.
                                     </Header>
                                     <p style={{ fontSize: '1.33em' }}>
-                                        <Image avatar src="/images/avatar/large/nan.jpg" />
-                                        <b>Nan</b> Chief Fun Officer Acme Toys
+                                        <Icon name={'user circle'} />
+                                        <b>David Michael Starsky</b> - Police Detective
                                     </p>
                                 </Grid.Column>
                             </Grid.Row>
@@ -276,60 +266,37 @@ const HomePage: React.FC = () => {
                                 Breaking The Grid, Grabs Your Attention
                             </Header>
                             <p style={{ fontSize: '1.33em' }}>
-                                Instead of focusing on content creation and hard work, we have learned how to master the art of doing nothing by providing
-                                massive amounts of whitespace and generic content that can seem massive, monolithic and worth your attention.
+                                Instead of focusing on manual calculations and hard work, we have learned how to master the art of the user doing nothing and
+                                getting lots in return! By using massive amounts of artificial intelligence algorithms we will definitely be worth of your
+                                attention.
                             </p>
-                            <Button as="a" size="large">
-                                Read More
-                            </Button>
-
                             <Divider as="h4" className="header" horizontal style={{ margin: '3em 0em', textTransform: 'uppercase' }}>
-                                <a href="#">Case Studies</a>
+                                Case Studies
                             </Divider>
 
                             <Header as="h3" style={{ fontSize: '2em' }}>
-                                Did We Tell You About Our Bananas?
+                                Save Your Precious Time!
                             </Header>
                             <p style={{ fontSize: '1.33em' }}>
-                                Yes I know you probably disregarded the earlier boasts as non-sequitur filler content, but its really true. It took years of
-                                gene splicing and combinatory DNA research, but our bananas can really dance.
+                                Yes I know you probably disregarded the earlier boasts as non-sequitur filler content, but it is really true. It took years of
+                                research, but our scheduling methods really are engineered with the finest programming code.
                             </p>
-                            <Button as="a" size="large">
-                                Im Still Quite Interested
-                            </Button>
                         </Container>
                     </Segment>
 
                     <Segment inverted vertical style={{ padding: '5em 0em' }}>
                         <Container>
-                            <Grid divided inverted stackable>
-                                <Grid.Row>
-                                    <Grid.Column width={3}>
-                                        <Header inverted as="h4" content="About" />
-                                        <List link inverted>
-                                            <List.Item as="a">Sitemap</List.Item>
-                                            <List.Item as="a">Contact Us</List.Item>
-                                            <List.Item as="a">Religious Ceremonies</List.Item>
-                                            <List.Item as="a">Gazebo Plans</List.Item>
-                                        </List>
-                                    </Grid.Column>
-                                    <Grid.Column width={3}>
-                                        <Header inverted as="h4" content="Services" />
-                                        <List link inverted>
-                                            <List.Item as="a">Banana Pre-Order</List.Item>
-                                            <List.Item as="a">DNA FAQ</List.Item>
-                                            <List.Item as="a">How To Access</List.Item>
-                                            <List.Item as="a">Favorite X-Men</List.Item>
-                                        </List>
-                                    </Grid.Column>
-                                    <Grid.Column width={7}>
-                                        <Header as="h4" inverted>
-                                            Footer Header
-                                        </Header>
-                                        <p>Extra space for a call to action inside the footer that could help re-engage users.</p>
-                                    </Grid.Column>
-                                </Grid.Row>
-                            </Grid>
+                            <Header as="h4" inverted>
+                                Starsky Scheduling Management
+                            </Header>
+                            <p>
+                                Made with <Icon name={'heart'} />
+                                in Slovenia by <a href={'https://github.com/peroxy'}>peroxy</a>.
+                            </p>
+                            <p>
+                                <Icon name={'github'} /> This is an open-source project, check us out on
+                                <a href={'https://github.com/peroxy/starsky-backend'}> GitHub</a>!
+                            </p>
                         </Container>
                     </Segment>
                 </ResponsiveContainer>
