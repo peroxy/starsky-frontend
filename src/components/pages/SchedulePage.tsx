0f64d9fa3161882e60a63cd5af111eef47a015b4
peroxy <peroxy@users.noreply.github.com>
2021-08-03 16:13:44 +0200

    Add auto solve functionality to schedule page

--- src/components/pages/SchedulePage.tsx
+++ src/components/pages/SchedulePage.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useHistory, useLocation, useParams } from 'react-router-dom';
-import { Button, Dimmer, Divider, Form, Grid, GridColumn, Icon, Loader, Segment } from 'semantic-ui-react';
+import { Button, Dimmer, Divider, Form, Grid, GridColumn, Icon, Loader, Progress, Segment } from 'semantic-ui-react';
 import { Helmet } from 'react-helmet';
 import { ActiveMenuItem, NavigationBar } from '../NavigationBar';
 import { useAuth } from '../AuthProvider';
@@ -20,7 +20,8 @@ import { Scheduler } from '../Scheduler';
 import { ConfirmActionModal } from '../modals/ConfirmActionModal';
 
 export const SchedulePage: React.FC = () => {
-    const [loading, setLoading] = useState({ initialLoad: true, processing: false });
+    const [loading, setLoading] = useState({ initialLoad: true, processing: false, autoSolve: false });
+    const [solvingPercentage, setSolvingPercentage] = useState(0);
     const [authenticatedUser, setAuthenticatedUser] = useState<UserResponse | null>(null);
     const [schedule, setSchedule] = useState<ScheduleResponse | null>(null);
     const [teams, setTeams] = useState<TeamResponse[]>([]);
@@ -80,95 +81,91 @@ export const SchedulePage: React.FC = () => {
                 console.error(responseToString(reason));
                 setNotFound(true);
             })
-            .finally(() => setLoading({ initialLoad: false, processing: false }));
+            .finally(() => setLoading({ initialLoad: false, processing: false, autoSolve: false }));
     }
 
-    function getFirstFormColumn() {
-        return (
-            <>
-                <Form.Input
-                    defaultValue={schedule?.scheduleName}
-                    labelPosition="left"
-                    label="Schedule name:"
-                    name={formScheduleName}
-                    required
-                    minLength={1}
-                    placeholder="My Schedule"
-                    icon="calendar"
-                    iconPosition="left"
-                />
-                <Form.Field label="Date range:" required style={{ marginBottom: 0 }} />
-                <DatePicker
-                    selectsRange
-                    startDate={dateRange.start}
-                    endDate={dateRange.end}
-                    onChange={(update: [Date, Date]) => setDateRange({ start: update[0], end: update[1] })}
-                    placeholderText="Click to select date range"
-                    required
-                    wrapperClassName="datePicker"
-                    calendarStartDay={1}
-                />
-                <Form.Dropdown
-                    floating
-                    labeled
-                    placeholder="Select Team"
-                    search
-                    label="Team:"
-                    required
-                    error={showTeamFieldError}
-                    selection
-                    options={teams.map((team) => {
-                        return { text: team.name, key: team.id, value: team.id, icon: 'users' };
-                    })}
-                    defaultValue={schedule?.teamId}
-                    onChange={(event, data) => setSelectedTeamId(data.value as number)}
-                />
-            </>
-        );
-    }
+    const getFirstFormColumn = () => (
+        <>
+            <Form.Input
+                defaultValue={schedule?.scheduleName}
+                labelPosition="left"
+                label="Schedule name:"
+                name={formScheduleName}
+                required
+                minLength={1}
+                placeholder="My Schedule"
+                icon="calendar"
+                iconPosition="left"
+            />
+            <Form.Field label="Date range:" required style={{ marginBottom: 0 }} />
+            <DatePicker
+                selectsRange
+                startDate={dateRange.start}
+                endDate={dateRange.end}
+                onChange={(update: [Date, Date]) => setDateRange({ start: update[0], end: update[1] })}
+                placeholderText="Click to select date range"
+                required
+                wrapperClassName="datePicker"
+                calendarStartDay={1}
+            />
+            <Form.Dropdown
+                floating
+                labeled
+                placeholder="Select Team"
+                search
+                label="Team:"
+                required
+                error={showTeamFieldError}
+                selection
+                options={teams.map((team) => {
+                    return { text: team.name, key: team.id, value: team.id, icon: 'users' };
+                })}
+                defaultValue={schedule?.teamId}
+                onChange={(event, data) => setSelectedTeamId(data.value as number)}
+            />
+        </>
+    );
 
-    function getSecondFormColumn() {
-        return (
-            <>
-                <Form.Input
-                    defaultValue={schedule?.maxHoursPerEmployee}
-                    labelPosition="left"
-                    label="Max total hours per employee:"
-                    name={formMaxHoursPerEmployee}
-                    required
-                    placeholder="40"
-                    icon="user"
-                    iconPosition="left"
-                    type="number"
-                    min={1}
-                />
-                <Form.Input
-                    defaultValue={schedule?.maxHoursPerShift}
-                    labelPosition="left"
-                    label="Max hours per single shift:"
-                    name={formMaxHoursPerShift}
-                    required
-                    placeholder="8"
-                    icon="clock"
-                    iconPosition="left"
-                    type="number"
-                    min={1}
-                />
-                <Form.Input
-                    defaultValue={schedule?.maxShiftsPerEmployee}
-                    labelPosition="left"
-                    label="Max total shifts per employee:"
-                    name={formMaxShiftsPerEmployee}
-                    required
-                    placeholder="5"
-                    icon="user"
-                    iconPosition="left"
-                    type="number"
-                    min={1}
-                />
-            </>
-        );
-    }
+    const getSecondFormColumn = () => (
+        <>
+            <Form.Input
+                defaultValue={schedule?.maxHoursPerEmployee}
+                labelPosition="left"
+                label="Max total hours per employee:"
+                name={formMaxHoursPerEmployee}
+                required
+                placeholder="40"
+                icon="user"
+                iconPosition="left"
+                type="number"
+                min={1}
+            />
+            <Form.Input
+                defaultValue={schedule?.maxHoursPerShift}
+                labelPosition="left"
+                label="Max hours per single shift:"
+                name={formMaxHoursPerShift}
+                required
+                placeholder="8"
+                icon="clock"
+                iconPosition="left"
+                type="number"
+                min={1}
+            />
+            <Form.Input
+                defaultValue={schedule?.maxShiftsPerEmployee}
+                labelPosition="left"
+                label="Max total shifts per employee:"
+                name={formMaxShiftsPerEmployee}
+                required
+                placeholder="5"
+                icon="user"
+                iconPosition="left"
+                type="number"
+                min={1}
+            />
+        </>
+    );
 
     const onSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
@@ -179,7 +176,7 @@ export const SchedulePage: React.FC = () => {
         }
 
         setError(getErrorNotOccurred());
-        setLoading({ initialLoad: false, processing: true });
+        setLoading({ initialLoad: false, processing: true, autoSolve: false });
         const formData = new FormData(event.currentTarget);
         const scheduleName = formData.get(formScheduleName) as string;
         const maxEmployeeHours = parseInt(formData.get(formMaxHoursPerEmployee) as string);
@@ -221,33 +218,65 @@ export const SchedulePage: React.FC = () => {
             })
             .catch(async (reason) => setError(await getErrorDetails(reason)))
             .finally(() => {
-                setLoading({ initialLoad: false, processing: false });
+                setLoading({ initialLoad: false, processing: false, autoSolve: false });
                 setShowTeamFieldError(false);
             });
     };
 
-    async function onDelete() {
+    const onDelete = async () => {
         if (!schedule) {
             return;
         }
 
-        setLoading({ initialLoad: false, processing: true });
+        setLoading({ initialLoad: false, processing: true, autoSolve: false });
         setError(getErrorNotOccurred());
 
         await apis.scheduleApi
             .deleteSchedule({ scheduleId: schedule.id })
             .then(() => history.push(SCHEDULES_ROUTE, authenticatedUser))
             .catch(async (reason) => setError(await getErrorDetails(reason)))
-            .finally(() => setLoading({ initialLoad: false, processing: false }));
-    }
+            .finally(() => setLoading({ initialLoad: false, processing: false, autoSolve: false }));
+    };
 
-    function getScheduleButton() {
+    const onAutoSolveConfirmed = async () => {
+        if (!schedule) {
+            return;
+        }
+
+        setLoading({ processing: false, initialLoad: false, autoSolve: true });
+
+        const increaseProgress = setInterval(() => {
+            setSolvingPercentage((prevState) => prevState + 0.5);
+        }, 55);
+
+        const solvedAssignments = await apis.scheduleApi
+            .solveScheduleById({ scheduleId: schedule.id })
+            .catch(async (reason) => setError(await getErrorDetails(reason)));
+
+        if (!solvedAssignments) {
+            setLoading({ initialLoad: false, processing: false, autoSolve: false });
+            clearInterval(increaseProgress);
+            setSolvingPercentage(0);
+            return;
+        }
+
+        await apis.employeeAssignmentApi
+            .putEmployeeAssignment({ scheduleId: schedule.id, putEmployeeAssignmentRequest: solvedAssignments })
+            .catch(async (reason) => setError(await getErrorDetails(reason)))
+            .finally(() => {
+                clearInterval(increaseProgress);
+                setSolvingPercentage(0);
+                setLoading({ initialLoad: false, processing: false, autoSolve: false });
+            });
+    };
+
+    const getScheduleButton = () => {
         let content = 'Create';
         if (schedule) {
             content = 'Save';
         }
         return (
-            <Grid columns={2}>
+            <Grid columns={'equal'}>
                 <GridColumn>
                     {schedule && (
                         <ConfirmActionModal
@@ -255,16 +284,29 @@ export const SchedulePage: React.FC = () => {
                             message={`Are you sure you want to delete this schedule '${schedule.scheduleName}'? This will also delete any shifts/assignments.`}
                             icon={<Icon name={'trash'} />}
                             onConfirm={() => onDelete()}
-                            trigger={<Button content="Delete" icon="trash" negative floated={'left'} />}
+                            trigger={<Button content="Delete" icon="trash" negative floated={'left'} type={'button'} disabled={loading.autoSolve} />}
+                        />
+                    )}
+                </GridColumn>
+                <GridColumn textAlign={'center'}>
+                    {schedule && (
+                        <ConfirmActionModal
+                            title={'Auto-solve schedule'}
+                            message={`Automatically solve the schedule and suggest employee assignments for the created shifts.
+                            Make sure you have created shifts for this schedule.
+                            Please note that this will delete any existing employee assignments that might exist.`}
+                            icon={<Icon name={'bolt'} />}
+                            onConfirm={onAutoSolveConfirmed}
+                            trigger={<Button content="Auto-solve schedule" primary icon="bolt" type={'button'} disabled={loading.autoSolve} />}
                         />
                     )}
                 </GridColumn>
                 <GridColumn>
-                    <Button content={content} icon="checkmark" positive floated={'right'} type="submit" />
+                    <Button content={content} icon="checkmark" positive floated={'right'} type="submit" disabled={loading.autoSolve} />
                 </GridColumn>
             </Grid>
         );
-    }
+    };
 
     const getLayout = () => {
         let segmentClass = '';
@@ -292,11 +334,23 @@ export const SchedulePage: React.FC = () => {
         }
         return (
             <>
-                <Segment as={Form} name={formName} className={segmentClass} loading={loading.processing} onSubmit={onSubmit}>
+                <Segment as={Form} name={formName} className={segmentClass} loading={loading.processing} onSubmit={onSubmit} disabled={loading.autoSolve}>
                     {segmentChildren}
                     {error.occurred ? <ErrorModal error={error} onOkClick={() => setError(getErrorNotOccurred())} /> : null}
                 </Segment>
-                {schedule ? <Scheduler schedule={schedule} employees={teamMembers} /> : null}
+                {loading.autoSolve ? (
+                    <Progress
+                        label={'Please wait...'}
+                        autoSuccess
+                        indicating
+                        size={'large'}
+                        percent={solvingPercentage}
+                        className={'scheduling'}
+                        style={{ margin: 'auto', width: '50%' }}
+                    />
+                ) : schedule ? (
+                    <Scheduler schedule={schedule} employees={teamMembers} />
+                ) : null}
             </>
         );
     };
